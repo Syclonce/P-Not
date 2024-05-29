5fcf4baf05a502af037ca1cc90e3e3edb45bee87
Syclonce <Syclonce@gmail.com>
2024-05-29 17:05:47 +0700

    update dompdf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Middleware\CheckLicense;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
+use barryvdh\DomPDF\Facade;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
-            'check.license' => CheckLicense::class
+            'PDF' => Facade::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
