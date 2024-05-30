f2a1227e3889251a9ea8957617ba0cb54909f728
Syclonce <Syclonce@gmail.com>
2024-05-30 16:41:44 +0700

    slove code

--- database/seeders/KecamatanSeeder.php
+++ database/seeders/KecamatanSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class WilayahSeeder extends Seeder
+class KecamatanSeeder extends Seeder
 {
     /**
      * Run the database seeds.
