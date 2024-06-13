ccc1e3c22bfd260da7135021373fcbd2cc726547
Syclonce <Syclonce@gmail.com>
2024-06-13 16:09:27 +0700

    clear

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\PemilikController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return view('welcome');
 });
 
 // Route::get('/dashboard', function () {
